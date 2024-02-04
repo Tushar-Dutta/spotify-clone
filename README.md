23681f0830926a895571b7cae1287b06f1b114e0
Tushar-Dutta <tushardutta_20242@aitpune.edu.in>
2024-02-04 19:18:00 +0530

    Final Build

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the Spotify Clone App with the Login Functionality in React.
+
+To access the site: https://spotify-clone-c5a66.web.app
---
+++ src/spotify.js
@@ -0,0 +1,29 @@
+// https://developer.spotify.com
+// documentation/web-playback-sdk
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "https://spotify-clone-c5a66.web.app";
+const clientId = "6eb586a95863452181275686e0f77186";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
